18ee1bb9b4ebf4268c226453ec6b7b89fd5ab6ae
bjgill33 <104100681+bjgill33@users.noreply.github.com>
2024-05-05 19:08:47 -0400

    Add files via upload

---
+++ CarLot.java
@@ -0,0 +1,99 @@
+package csc251.team.project;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class CarLot {
+	private ArrayList<Car> inventory;
+	private int numberOfCars = 0;
+	private int capacity = 0;
+	
+	public CarLot() { 
+		this(100); 
+	}
+	
+	public CarLot(int capacity) {
+		this.capacity = capacity;
+		this.inventory = new ArrayList<>();
+	}
+	
+	public void addCar(String id, int mileage, int mpg, double cost, double salesPrice) {
+		if (numberOfCars < capacity) {
+			inventory.add(new Car(id, mileage, mpg, cost, salesPrice));
+			numberOfCars++;
+		}
+	}
+	
+	public ArrayList<Car> getInventory() {
+		return new ArrayList<>(inventory);
+	}
+	
+	public Car findCarByIdentifier(String identifier) {
+		for (Car aCar : inventory) {
+			if (aCar.getId().equals(identifier)) {
+				return aCar;
+			}
+		}
+		return null;
+	}
+	
+	public void sellCar(String identifier, double priceSold) throws IllegalArgumentException {
+		Car aCar = this.findCarByIdentifier(identifier);
+		if (aCar != null) {
+			aCar.sellCar(priceSold);
+		} else {
+			throw new IllegalArgumentException("No car with identifier " + identifier);
+		}
+	}
+	
+	public ArrayList<Car> getCarsInOrderOfEntry() { 
+		return this.inventory; 
+	}
+	
+	public ArrayList<Car> getCarsSortedByMPG() {
+		ArrayList<Car> allCars = new ArrayList<>(inventory);
+		Collections.sort(allCars, (Car c1, Car c2) -> c2.compareMPG(c1));
+		return allCars;
+	}
+	
+	public Car getCarWithBestMPG() {
+		Car rtn = null;
+		int bestMpg = -1;
+		for (Car aCar : inventory) {
+			if (aCar.getMpg() > bestMpg) {
+				bestMpg = aCar.getMpg();
+				rtn = aCar;
+			}
+		}
+		return rtn;
+	}
+	
+	public Car getCarWithHighestMileage() {
+		Car rtn = null;
+		int highestMileage = -1;
+		for (Car aCar : inventory) {
+			if (aCar.getMileage() > highestMileage) {
+				highestMileage = aCar.getMileage();
+				rtn = aCar;
+			}
+		}
+		return rtn;
+	}
+	
+	public double getAverageMpg() {
+		double totalMpg = 0D;
+		for (Car aCar : inventory) {
+			totalMpg += aCar.getMpg();
+		}
+		return totalMpg / this.numberOfCars;
+	}
+	
+	public double getTotalProfit() {
+		double profit = 0D;
+		for (Car aCar : inventory) {
+			profit += (aCar.isSold() ? aCar.getProfit() : 0);
+		}
+		return profit;
+	}
+	
+}
---
+++ ProjectTester.java
@@ -0,0 +1,54 @@
+package csc251.team.project;
+
+
+public class ProjectTester {
+
+	public static void main(String[] args) {
+		// create a new instance of CarLot and add 4 cars
+		CarLot newLot = new CarLot();
+		newLot.addCar("Ford Taurus", 5000, 23, 8000D, 9900D);
+		newLot.addCar("Chevy Blazer", 15000, 19, 12000D, 14500D);
+		newLot.addCar("Honda Civic", 12300, 25, 6500D, 10000D);
+		newLot.addCar("KIA Sorento", 22000, 28, 13500D, 15500D);
+		
+		// Display inventory
+		System.out.println("Inventory: ");
+		for (Car car: newLot.getInventory()) {
+			System.out.println(car);
+		}
+		
+		System.out.println("\nAverage MPG: " + newLot.getAverageMpg());
+		System.out.println("Total Profit: " + newLot.getTotalProfit());
+		System.out.println("Car with the best MPG: " + newLot.getCarWithBestMPG());
+		System.out.println("Car with the highest mileage: " + newLot.getCarWithHighestMileage());
+		newLot.sellCar("KIA Sorento", 15500.0D);
+		System.out.println("Total profit after selling one car: " + newLot.getTotalProfit());
+		
+		// sell another car at a loss to test getTotalProfit() method
+		newLot.sellCar("Ford Taurus", 7000.0D);
+		System.out.println("Total profit after selling two cars: " + newLot.getTotalProfit());
+		
+		// display inventory to test that isSold() method works
+		System.out.println("\nInventory: ");
+		for (Car car: newLot.getInventory()) {
+			System.out.println(car);
+		}
+		
+		// test by selling a car already sold
+		newLot.sellCar("Ford Taurus", 7000.0D);
+		
+		// Inventory not affected by trying to sell car already sold
+		System.out.println("\nInventory: ");
+		for (Car car: newLot.getInventory()) {
+			System.out.println(car);
+		}
+		
+		// TEST does selling already sold car affect the profits
+		System.out.println("\nTotal Profit: " + newLot.getTotalProfit()); // has no effect on profits
+		
+		System.out.println(newLot.getCarsSortedByMPG());
+		System.out.println(newLot.getCarsInOrderOfEntry());
+		
+	}
+
+}
